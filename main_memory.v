d2df9704d29eef81e24f92e6265062eb8f4390c9
Romir Shetty <romir2005shetty@gmail.com>
2026-09-15 22:32:42 +0530

    final updates

--- main_memory.v
+++ main_memory.v
@@ -1,25 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 15.09.2026 09:43:23
-// Design Name: 
-// Module Name: main_memory
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module main_memory(
     input clk,
     input reset,
